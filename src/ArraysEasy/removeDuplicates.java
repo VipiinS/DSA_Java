011bec65e63d99eda771a1841f70accf220878cf
VipiinS <Vipinshanmugam@gmail.com>
2023-11-19 12:11:09 +0530

    findingDuplicates

---
+++ src/ArraysEasy/removeDuplicates.java
@@ -0,0 +1,25 @@
+package ArraysEasy;
+
+import java.util.Arrays;
+
+public class removeDuplicates {
+    public static void main(String[] args) {
+        int[] nums = {1,1,2};
+        System.out.println(removeDuplicatesInaArray(nums));
+        System.out.println(Arrays.toString(nums));
+    }
+    public static int removeDuplicatesInaArray(int[] nums) {
+        if(nums.length == 1) return 1;
+        int l = 0;
+        int r = 1;
+
+        while(r< nums.length){
+            if(nums[r] == nums[r-1])
+                r++;
+            if(nums[r] != nums[r-1]){
+                nums[++l] = nums[r++];
+            }
+        }
+        return l+1;
+    }
+}
